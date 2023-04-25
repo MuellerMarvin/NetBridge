fbede7d36b9ebe63b8903a7ddafb91e2deac6be7
Marvin Müller <marvin.mueller@code.berlin>
2023-04-25 15:37:55 +0200

    Rename INetTask, make it a class, not an interfacce

---
+++ NetBridge/Networking/Models/NetworkTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBridge.Networking.Models
+{
+    public struct NetworkTask
+    {
+        public int Id { get; set; }
+        public int TaskType { get; set; }
+        public Object[] Payload { get; set; }
+    }
+}
